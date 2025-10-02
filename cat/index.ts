630322d487730209ccacbc369417c9b88d667fbd
Khushi Roy <roykhushi432@gmail.com>
2025-10-03 00:51:21 +0530

    multi file support

--- cat/index.ts
+++ cat/index.ts
@@ -1,18 +1,30 @@
-//stdout 
+//stdout
 //open file → get readable stream then connect readable stream → process.stdout
 // not using fs.readfilesync => loads entire file in mem at once and unix cmds dont work like that
-//using streams -> files read line by line and in chunks 
+//using streams -> files read line by line and in chunks
 
 import * as fs from "fs";
 
-const file = "file.txt";
+// const file = "file.txt";
+const files = process.argv.slice(2);
 
-//creating a readable stream
-const rs = fs.createReadStream(file,{encoding:"utf-8"});
+if (files.length == 0) {
+  console.error("No files provided");
+  process.exit(1);
+}
 
-rs.pipe(process.stdout);//chunks of file data sent directly to terminal
+files.forEach((file) => {
+  //creating a readable stream
+  const rs = fs.createReadStream(file, { encoding: "utf-8" });
 
-rs.on("error",(e)=>{
+  rs.pipe(process.stdout); //chunks of file data sent directly to terminal
+
+  rs.on("error", (e) => {
     console.error(`error occ ${e.message}`);
     process.exit(1);
+  });
 });
+
+
+
+
